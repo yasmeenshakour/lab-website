024403211ba65a72386e0899f609d78e46c3dee9
yasmeenshakour <33812013+yasmeenshakour@users.noreply.github.com>
2020-09-02 13:11:07 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// dropdown menu for header
+// function dropDown(){
+// 	document.querySelector("#drop-down").classList.toggle("show");
+// }
+
+function dropDown() {
+  document.getElementById("drop-down").classList.toggle("show");
+}
